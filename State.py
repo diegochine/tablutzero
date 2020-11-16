1d6a2c344cd4c976f031d1d3d46a6d3b15eca77a
Diego Chinellato <chine.diego@gmail.com>
2020-11-16 23:07:06 +0100

    added hash computation

--- State.py
+++ State.py
@@ -7,6 +7,12 @@ class State:
         self.board = board
         self.turn = turn
 
+    def __hash__(self):
+        return hash((tuple(tuple(row) for row in self.board), self.turn))
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
     def get_checkers(self):
         """ return positions of checkers that can be moved in this state """
         checkers = set(tuple(x) for x in np.argwhere(self.board == self.turn))
