4a8d59c28f3f1ec4877364bb7fb05413e6842383
Diego Chinellato <chine.diego@gmail.com>
2020-11-19 11:47:56 +0100

    Create loggers.py

---
+++ loggers.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def setup_logger(name, file, level=logging.INFO):
+    formatter = logging.Formatter('{time} {level} {info}')
+    file_handler = logging.FileHandler(file)
+    file_handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+
+    return logger
+
+
+logger_mcts = setup_logger('MCTS', 'logs/MCTS.log')
+logger_main = setup_logger('__main__', 'logs/main.log')
+logger_nnet = setup_logger('neuralnet', 'logs/nnet.log')
